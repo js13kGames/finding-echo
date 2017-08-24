2c976ffa6ee82936a8b3964a22b04bd4a7713352
Marco Segreto <marco.segreto@gsa.gov>
2017-08-24 16:26:39 -0700

    Play around with how walls will work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 
+import Manager from './manager';
+import Wall from './wall';
+
 console.log('it works');
+
+const tW = new Wall(0, 0, 20, 2);
+const rW = new Wall(20, 0, 2, 20);
+const lW = new Wall(0, 0, 2, 20);
+const bW = new Wall(0, 20, 20, 2);
+
+manager.addEntity(tW);
+manager.addEntity(rW);
+manager.addEntity(lW);
+manager.addEntity(bW);
