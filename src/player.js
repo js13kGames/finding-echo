52630ea8651e0e191a328b385c798e3fa23cf1a0
Marco Segreto <msegreto@miceover.com>
2017-09-04 20:56:04 -0700

    Add the reverb to the echo sound

--- src/player.js
+++ src/player.js
@@ -5,30 +5,41 @@ import EventEmitter from 'event-emitter-es6';
 import Dispatcher from './dispatcher';
 import Vector from './vector';
 
-const sfxDolphin = [2,,0.1199,0.28,0.2837,0.68,0.05,,-0.92,0.8,0.4499,-0.4399,,,,0.6,-0.1599,-0.62,0.28,-0.5799,0.1,0.2399,-0.3799,0.5]
-const sfxBaby = [0,,0.1812,,0.1349,0.4524,,0.2365,,,,,,0.0819,,,,,1,,,,,0.5];
-const sfxBabyURL = jsfxr(sfxBaby);
-
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
+const sfxDolphin = [2,,0.1199,0.28,0.2837,0.68,0.05,,-0.92,0.8,0.4499,-0.4399,,,,0.6,-0.1599,-0.62,0.28,-0.5799,0.1,0.2399,-0.3799,0.5]
+const sfxBaby = [0,,0.1812,,0.1349,0.4524,,0.2365,,,,,,0.0819,,,,,1,,,,,0.5];
+
+const sfxDolphinURL = jsfxr(sfxDolphin);
+const dolphinAudio = new Audio();
+dolphinAudio.src = sfxDolphinURL;
+dolphinAudio.crossOrigin = "anonymous";
+const dolphinSource = audioCtx.createMediaElementSource(dolphinAudio);
+const gainNode = audioCtx.createGain();
+const convolverGain = audioCtx.createGain();
+const convolver = audioCtx.createConvolver();
+dolphinSource.connect(gainNode);
+dolphinSource.connect(convolverGain);
+gainNode.connect(audioCtx.destination);
+convolverGain.connect(convolver);
+
+
+const sfxBabyURL = jsfxr(sfxBaby);
 const audio = new Audio();
 audio.src = sfxBabyURL;
 audio.loop = true;
 audio.crossOrigin = "anonymous";
-
 const source = audioCtx.createMediaElementSource(audio);
-
 const panNode = audioCtx.createPanner();
 panNode.coneOuterGain = 0.4;
 panNode.coneOuterAngle = 240;
 panNode.coneInnerAngle = 0;
 panNode.refDistance = 20;
-
 source.connect(panNode);
 panNode.connect(audioCtx.destination);
-
 audioCtx.listener.setPosition(0, 0, 0);
 panNode.setPosition(40, 40, -0.5);
+
 
 class Player extends EventEmitter {
   constructor(x, y) {
@@ -65,7 +76,9 @@ class Player extends EventEmitter {
         this.rotate(-90);
         break;
       case 't':
-        console.log('player : emit echo');
+        gainNode.gain.value = 1;
+        convolverGain.gain.value = 0;
+        dolphinAudio.play();
         Dispatcher.emitSync('ECHO');
         break;
 
@@ -75,7 +88,11 @@ class Player extends EventEmitter {
   }
 
   onEchoFound(data) {
-    console.log('player : echo found', data);
+    this.timer = setTimeout(() => {
+      gainNode.gain.value = 0.2;
+      convolverGain.gain.value = 1;
+      dolphinAudio.play();
+    }, data.distance * 10);
   }
 
   move(x, y) {
